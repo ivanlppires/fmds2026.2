38a50265e23d150ce4b49c6f668f9561c3470311
Ivan Pires <ivanpires@gmail.com>
2026-08-19 20:57:19 -0400

    v-bin e reatiividade

--- aula2/src/App.vue
+++ aula2/src/App.vue
@@ -1,21 +1,27 @@
 <template>
-  <!-- o que vai ser mostrado (html) -->
-  <h1>Lista de Produtos</h1>
-  <ul>
-    <li>Maçã</li>
-    <li>Banana</li>
-    <li>Laranja</li>
-  </ul>
+  <!-- v-bind (:) = liga as propriedades do script com o template -->
+  <!-- v-on (@) = A partir d template chama o método do script  -->
+  <!-- v-model (two-way data-bind) = liga o valor de um input com uma variável do script -->
+  <!-- interpolação {{  }} = Exibe o valor de uma variável do script no template -->
+  
+  <h1>{{ nome }}</h1>
+  <p :style="{ color: cor }">{{  nome  }}</p>
+
+  <input type="button" :value="cor" @click="mudacor" />
+
 </template>
 
 <script setup>
-// o que vai ser mostrado (javascript)
-window.alert("Bem-vindo ao Vue.js!")
+  import { ref } from 'vue';
+  const nome = "João";
+  const cor = ref('orange');
+
+  const mudacor = () => {
+    window.alert("Mudando a cor");
+    cor.value = cor.value === 'orange' ? 'blue' : 'orange';
+  }
+
 </script>
 
 <style scoped>
-/* o que vai ser mostrado (css) */
-h1{
-  color:blue;
-}
 </style>
