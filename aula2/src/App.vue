18ed321f4ebad7e7c30765b5a8eb3f13a2931833
Ivan Pires <ivanpires@gmail.com>
2026-08-19 20:22:09 -0400

    aula02

---
+++ aula2/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- o que vai ser mostrado (html) -->
+  <h1>Lista de Produtos</h1>
+  <ul>
+    <li>Maçã</li>
+    <li>Banana</li>
+    <li>Laranja</li>
+  </ul>
+</template>
+
+<script setup>
+// o que vai ser mostrado (javascript)
+window.alert("Bem-vindo ao Vue.js!")
+</script>
+
+<style scoped>
+/* o que vai ser mostrado (css) */
+h1{
+  color:blue;
+}
+</style>
