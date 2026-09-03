6818557a46b561a2e76e642754cab19860f0a525
Ivan Pires <ivanpires@gmail.com>
2026-09-02 20:43:16 -0400

    remover

--- aula3/src/App.vue
+++ aula3/src/App.vue
@@ -5,23 +5,30 @@
  */
 import { ref } from "vue";
 
-const pecas = ref([
-  { id: 1, nome: "Junta homocinética", quantidade: 10, preco: 100 },
-  { id: 2, nome: "Semi eixo", quantidade: 5, preco: 200 },
-  { id: 3, nome: "Bieleta", quantidade: 0, preco: 50 },
-  { id: 4, nome: "Amortecedor", quantidade: 15, preco: 300 },
-]);
 const perfumes = ref([
-  { id: 1, nome: "212 Vip", quantidade: 20, preco: 150 },
-  { id: 2, nome: "Al Noble", quantidade: 0, preco: 250 },
-  { id: 3, nome: "Joomp", quantidade: 15, preco: 300 },
-  { id: 4, nome: "Cebolinha", quantidade: 15, preco: 300 },
+  { id: 100, nome: "212 Vip", quantidade: 20, preco: 150 },
+  { id: 205, nome: "Al Noble", quantidade: 0, preco: 250 },
+  { id: 312, nome: "Joomp", quantidade: 15, preco: 300 },
+  { id: 440, nome: "Cebolinha", quantidade: 15, preco: 300 },
 ]);
 
-const logado = ref(false);
+const logado = ref(true);
 const logar = () => {
-  logado.value = true;
+  if (user.value.name === 'joao'
+    && user.value.pass === '123')
+    logado.value = true;
+  else
+    alert('usuario ou senha incorretos');
 };
+const user = ref({ name: '', pass: '' });
+const removerId = (id) => {
+  // retorne todos com id diferente do id passado
+  perfumes.value = perfumes.value.filter(p => p.id !== id);
+}
+const removerIndex = (i) => {
+  // na posição i remova 1 elemento 
+  perfumes.value.splice(i, 1);
+}
 </script>
 
 <template>
@@ -33,8 +40,8 @@ const logar = () => {
       <p>Faça Login</p>
       <i>Faça o login se o usuario for joao e senha 123</i>
       <br>
-      <input type="text" placeholder="usuario" v-model="usuario">
-      <br>
+      <input type="text" placeholder="usuario" v-model="user.name">
+      <input type="password" placeholder="senha" v-model="user.pass" />
       <button @click="logar">Entrar</button>
     </section>
     <section v-else>
@@ -57,11 +64,16 @@ const logar = () => {
             <th>Nome</th>
             <th>Quantidade</th>
             <th>Preço</th>
+            <th>Ações</th>
           </tr>
-          <tr v-for="p in perfumes" :key="p.id">
+          <tr v-for="(p, index) in perfumes" :key="p.id">
             <td v-html="p.nome"></td>
             <td v-html="p.quantidade"></td>
             <td v-html="p.preco"></td>
+            <td>
+              <button @click="removerId(p.id)">Remover</button>
+              <button @click="removerIndex(index)">Remover</button>
+            </td>
           </tr>
         </table>
         <br><br>
